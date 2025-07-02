084f20300aaeaeca2d235b33794293494d0b79f3
Albin Löfmark <albinlofmark01@gmail.com>
2025-07-02 22:03:33 +0200

    Implemented "Person" class with getters, setters and a "getSummary" method.

--- src/main/java/se/lofmark/Main.java
+++ src/main/java/se/lofmark/Main.java
@@ -3,5 +3,7 @@ package se.lofmark;
 public class Main {
     public static void main(String[] args) {
 
+        Person Albin = new Person(42, "Albin", "Löfmark", "albinlofmark01@gmail.com");
+        System.out.println(Albin.getSummary());
     }
 }
--- src/main/java/se/lofmark/Person.java
+++ src/main/java/se/lofmark/Person.java
@@ -1,4 +1,81 @@
 package se.lofmark;
 
 public class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Person(int id, String firstName, String lastName, String email) {
+        setId(id);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+    }
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+
+    public void setId(int id) {
+
+        if (id > 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("ID cannot be negative, 0 or empty");
+        }
+    }
+
+    public void setFirstName(String firstName) {
+
+        if (firstName == null || firstName.trim().isEmpty()) {
+            throw new IllegalArgumentException("First name cannot be null or empty");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null || lastName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Last name cannot be null or empty");
+        }
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        if (email == null || email.trim().isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+        this.email = email;
+    }
+
+    public String getSummary() {
+
+        StringBuilder summary = new StringBuilder();
+
+        summary.append("Person Info -> ID: ").append(id)
+                .append(", First name: ").append(firstName)
+                .append(", Last name: ").append(lastName)
+                .append(", Email: ").append(email);
+
+        return summary.toString();
+    }
+
+
 }
